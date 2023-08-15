87daaa4563db8d825440dc8f53c16b267bac5b97
Hamza Gedikkaya <hamza.gedikkaya@iwallet.com.tr>
2023-08-15 10:13:58 +0300

    Test Articles Added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Article.create(title: 'Article1', content: 'lorem')
+Article.create(title: 'Article2', content: 'lorem ipsum')
+Article.create(title: 'Article3', content: 'lorem ipsum dolor')
+Article.create(title: 'Article4', content: 'lorem ipsum dolor sit amet')
