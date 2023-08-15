1df01f899039cb6175cdcf79673442e1822d03a1
Hamza Gedikkaya <hamza.gedikkaya@iwallet.com.tr>
2023-08-15 10:01:58 +0300

    model and controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # get 'articles/index'
+  # get 'articles/show'
+  # get 'articles/update'
+  # get 'articles/create'
+  # get 'articles/destroy'
+  resources :articles
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
