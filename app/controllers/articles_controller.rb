df6f28dd4f900e7916534e5f0cb2b8885b28a818
Hamza Gedikkaya <hamza.gedikkaya@iwallet.com.tr>
2023-08-16 14:46:08 +0300

    image added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -53,7 +53,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :content)
+    params.require(:article).permit(:title, :content, :avatar)
   end
 
 end
