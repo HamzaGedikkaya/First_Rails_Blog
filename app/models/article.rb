960354301aa7907a51c27e7e1b5651ff55b076e3
Hamza Gedikkaya <hamza.gedikkaya@iwallet.com.tr>
2023-08-28 14:47:25 +0300

    Added roles to seeds.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,17 +1,17 @@
 class Article < ApplicationRecord
 
-    #* AVATAR
-    has_one_attached :avatar
+  #* AVATAR
+  has_one_attached :avatar
     
-    #* RANSACK (SEARCH)
-    def self.ransackable_attributes(auth_object = nil)
-        ['title', 'content']
-    end
+  #* RANSACK (SEARCH)
+  def self.ransackable_attributes(auth_object = nil)
+      ['title', 'content']
+  end
 
-    def self.ransackable_associations(auth_object = nil)
-        []
-    end
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
 
-    #* COMMENTS
-    has_many :comments, dependent: :destroy
+  #* COMMENTS
+  has_many :comments, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,34 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+    #!Articles
 Article.create(title: 'Article1', content: 'lorem')
 Article.create(title: 'Article2', content: 'lorem ipsum')
 Article.create(title: 'Article3', content: 'lorem ipsum dolor')
 Article.create(title: 'Article4', content: 'lorem ipsum dolor sit amet')
+
+#     #!Users
+# users = [
+#     { email: 'admin@gmail.com', password: '123123' },
+#     { email: 'editor@gmail.com', password: '123123' },
+#     { email: 'author@gmail.com', password: '123123' },
+#     { email: 'user@gmail.com', password: '123123' },
+#     { email: 'user2@gmail.com', password: '123123' },
+
+#   ]
+
+# users.each do |user_params|
+#   user = User.find_or_create_by(email: user_params[:email]) do |u|
+#     u.password = user_params[:password]
+#   end
+#   puts "Kullanıcı #{user.email} oluşturuldu."
+# end
+
+    #!Roles
+    
+roles = ['admin', 'editor', 'author']
+
+roles.each do |role_name|
+  Role.find_or_create_by(name: role_name)
+  puts "Rol '#{role_name}' oluşturuldu."
+end
